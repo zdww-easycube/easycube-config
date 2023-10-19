ca1823e337801d28f971d0aacd08d1741dfd3e4d
右究 <aisowe@sina.com>
2023-10-19 09:48:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# easycube-config
+
+提供一些 EasyCube 打包自定义组件和构建项目时会用到的配置。
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare const globalDependencies: Record<string, string>
+declare const externals: string[]
+declare const widgetExternals: string[]
+
+export { externals, globalDependencies, widgetExternals }
